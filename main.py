0fdb9eac7f65d3d715a642790a170664944a2462
Mathieu Thomassin <mathieu.thomassin@eleve.ensai.fr>
2023-03-16 15:01:25 +0100

    function get news for country

--- main.py
+++ main.py
@@ -38,7 +38,7 @@ def get_news(topic, from_date, to_date, language='en',
         results.append(f"TITLE: ' {article['title']}, '\nDESCRIPTION: ', {article['description']}")
     return results
 
-print(get_news(topic="space", from_date='2023-2-27', to_date='2023-2-28'))
+# print(get_news(topic="space", from_date='2023-2-27', to_date='2023-2-28'))
 
 import requests
 
@@ -54,24 +54,18 @@ import requests
 #           '\nDESCRIPTION: ', article["description"])
 
 
-def get_news(country, language='en',
-            api_key='8e36ee183cb743169ac0a7f9e6d2904f'):
+def get_news_from_country(country, language='en', api_key='8e36ee183cb743169ac0a7f9e6d2904f'):
     """get news from website
 
     Parameters
     ----------
-    topic : string
-        _description_
-    from_date : date
-        _description_
-    to_date : date
-        _description_
+
     language : str, optional
         _description_, by default 'en'
     api_key : str, optional
         _description_, by default '8e36ee183cb743169ac0a7f9e6d2904f'
     """
-    url = f"https://newsapi.org/v2/everything?qInTitle={topic}&from={from_date}&to={to_date}&sortBy=popularity&language={language}&apiKey={api_key}"
+    url = f"https://newsapi.org/v2/top-headlines?country={country}&apiKey={api_key}"
     r = requests.get(url)
     content = r.json()
     articles = content["articles"]
@@ -79,3 +73,5 @@ def get_news(country, language='en',
     for article in articles:
         results.append(f"TITLE: ' {article['title']}, '\nDESCRIPTION: ', {article['description']}")
     return results
+
+print(get_news_from_country(country='us'))
